a74d0ab93767df58b4de9280f820fa9d9508fd56
Vidit Shah <vidit0210@gmail.com>
2020-10-08 12:11:02 +0530

    effecient

--- Array/Easy/1470_ShuffleTheArray.py
+++ Array/Easy/1470_ShuffleTheArray.py
@@ -1,3 +1,19 @@
+# class Solution(object):
+#     def shuffle(self, nums, n):
+#         """
+#         :type nums: List[int]
+#         :type n: int
+#         :rtype: List[int]
+#         """
+#         x = nums[:n]
+#         y = nums[n:]
+#         final = []
+#         for i in range(n):
+#             final.append(x[i])
+#             final.append(y[i])
+#         return final
+
+
 class Solution(object):
     def shuffle(self, nums, n):
         """
@@ -5,10 +21,12 @@ class Solution(object):
         :type n: int
         :rtype: List[int]
         """
-        x = nums[:n]
-        y = nums[n:]
-        final = []
-        for i in range(n):
-            final.append(x[i])
-            final.append(y[i])
-        return final
+        output = []
+        i = 0
+        j = n
+        while i < n:
+            output.append(nums[i])
+            output.append(nums[j])
+            i += 1
+            j += 1
+        return output
