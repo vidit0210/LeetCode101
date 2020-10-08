6516d9590fa8454966356f1a1f9ead0dfd5f25a8
Vidit Shah <vidit0210@gmail.com>
2020-10-08 11:39:35 +0530

    p2

--- Array/Easy/runningSum1D.py
+++ Array/Easy/runningSum1D.py
@@ -10,3 +10,12 @@ class Solution(object):
             sum += nums[i]
             new_arr.append(sum)
         return new_arr
+
+
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return [sum(nums[:i+1]) for i in range(len(nums))]
